e9fe3c1480e7cdb0b0a51ea9b1bcff3125fc6133
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2024-11-26 19:54:18 +0000

    Major Update -  build.sh - being refining for customization

--- build.sh
+++ build.sh
@@ -13,11 +13,20 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
-
+rpm-ostree install papirus-icon-theme
+rpm-ostree install qemu virt-manager edk2-ovmf
+rpm-ostree install podman
+rpm-ostree install sweet-gtk-theme obs-studio distrobox 
+# Gnome extension as well as X11 support as it is disabled by default on Fedora 41+
+rpm-ostree install gnome-shell-extension-gsconnect nautilus nautilus-gsconnect gnome-shell-extension-user-theme gnome-session-xsession xorg-x11-server-Xorg
+# FileLight from KDE is only temporary until a 
+rpm-ostree install filelight fastfetch
 # this would install a package from rpmfusion
-# rpm-ostree install vlc
+rpm-ostree install vlc
+
+#Driver installation - Nvidia refresh - Use 'akmod' driver which behaves likes the '-dkms' driver you see in arch based distros
+rpm-ostree install akmod-nvidia
 
 #### Example for enabling a System Unit File
-
+systemctl enable libvirtd
 systemctl enable podman.socket
