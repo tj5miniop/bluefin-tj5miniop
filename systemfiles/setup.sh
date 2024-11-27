b4f4c835038421dfccdba1fe1e1a4a77bd2f7c9c
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2024-11-27 19:34:36 +0000

    Update setup.sh

--- systemfiles/setup.sh
+++ systemfiles/setup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-mkdir /tmp/bluefin-custom
-cd /tmp/bluefin-custom
-
-curl -O https://raw.githubusercontent.com/tj5miniop/ublue-scripts/refs/heads/Main/bluefin/build.sh
-curl -O https://raw.githubusercontent.com/tj5miniop/ublue-scripts/refs/heads/Main/bluefin/setup.sh
-
-bash build.sh
-bash setup.sh
+gsettings set org.gnome.desktop.interface gtk-theme "Sweet-ambar-blue-dark"
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
+gsettings set org.gnome.desktop.wm.preferences theme "Sweet"
+gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/wallpaper.jpg
